6fab0559329c0fec42a7df75b7938a756261cd8c
clebercar <contatoclebercarvalho@gmail.com>
2021-06-26 19:05:37 -0300

    feat: login with google and firebase

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/modules/login/login_controller.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
@@ -15,6 +16,7 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
+    final controller = LoginController();
 
     return Scaffold(
       backgroundColor: AppColors.background,
@@ -60,7 +62,7 @@ class _LoginPageState extends State<LoginPage> {
                           const EdgeInsets.only(left: 40, right: 40, top: 40),
                       child: SocialLoginButton(
                         onTap: () {
-                          print('clicou');
+                          controller.googleSignIn(context);
                         },
                       ),
                     )
