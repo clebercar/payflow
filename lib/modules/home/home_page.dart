9b21265aa9bad5c4f7859af0fc8e867a74b6e16e
clebercar <contatoclebercarvalho@gmail.com>
2021-06-26 20:05:14 -0300

    feat(wip): creating home page

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,13 +1,96 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/modules/home/home_controller.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final controler = HomeController();
+  final pages = [
+    Container(color: Colors.red),
+    Container(color: Colors.blue),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
+      appBar: PreferredSize(
+        child: Container(
+          height: 152,
+          color: AppColors.primary,
+          child: Center(
+            child: ListTile(
+              title: Text.rich(
+                TextSpan(
+                    text: 'Olá, ',
+                    style: TextStyles.titleRegular,
+                    children: [
+                      TextSpan(
+                          text: 'Cleber',
+                          style: TextStyles.titleBoldBackground),
+                    ]),
+              ),
+              subtitle: Text(
+                'Mantenha suas contas em dia',
+                style: TextStyles.captionShape,
+              ),
+              trailing: Container(
+                width: 48,
+                height: 48,
+                decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(5)),
+              ),
+            ),
+          ),
+        ),
+        preferredSize: Size.fromHeight(152),
+      ),
+      body: pages[controler.currentPage],
+      bottomNavigationBar: Container(
+        height: 90,
+        child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          IconButton(
+              onPressed: () {
+                controler.setPage(0);
+                setState(() {});
+              },
+              icon: Icon(
+                Icons.home,
+                color: AppColors.primary,
+              )),
+          GestureDetector(
+            onTap: () {
+              print('clicou');
+            },
+            child: Container(
+              height: 56,
+              width: 56,
+              decoration: BoxDecoration(
+                  color: AppColors.primary,
+                  borderRadius: BorderRadius.circular(5)),
+              child: Icon(
+                Icons.add_box_outlined,
+                color: AppColors.background,
+              ),
+            ),
+          ),
+          IconButton(
+              onPressed: () {
+                controler.setPage(1);
+                setState(() {});
+              },
+              icon: Icon(
+                Icons.description_outlined,
+                color: AppColors.body,
+              )),
+        ]),
       ),
     );
   }
